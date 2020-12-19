17a612994fae3769043b4fbd2e30a4bd2ea9a770
Jaewoong <jaewoong.go@gmail.com>
2020-12-20 00:11:20 +0900

    Add echo comments

--- start_app.sh
+++ start_app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo DB Migration ..
+echo "DB Migration ..(1/3)"
 python manage.py makemigrations
 python manage.py migrate
 
-echo Collect static files ..
+echo "Collect static files ..(2/3)"
 python manage.py collectstatic --noinput
 
-echo Gunicorn start ..
+echo "Gunicorn start ..(3/3)"
 gunicorn dockertest.wsgi:application --bind 0.0.0.0:8000
