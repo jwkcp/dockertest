c9c5d68e7304d4b1763da88743f9ce0684e854ab
Jaewoong <jaewoong.go@gmail.com>
2020-12-19 16:21:04 +0900

    Fix and add initial script

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
-# install latest version of docker the lazy way
+# Install docker
 curl -sSL https://get.docker.com | sh
-
-# make it so you don't need to sudo to run docker commands
-usermod -aG docker ubuntu
+sudo usermod -aG docker ubuntu
 
 # Install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-############
-#mkdir ~/apps
-#
-############
+sudo mkdir /srv/docker
+sudo curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/jwkcp/dockertest/master/docker-compose.yml
 
-# copy the dockerfile into /srv/docker
-# if you change this, change the systemd service file to match
-# WorkingDirectory=[whatever you have below]
-#mkdir /srv/docker
-#curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/jwkcp/dockertest/master/docker-compose.yml
-
-# copy in systemd unit file and register it so our compose file runs
-# on system restart
-#curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/jwkcp/dockertest/master/docker-compose-app.service
-#cp ~/apps/dockertest/docker-compose-app.service /etc/systemd/system/docker-compose-app.service
-#systemctl enable docker-compose-app
+# Set systemd
+sudo curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/jwkcp/dockertest/master/docker-compose.yml
+sudo systemctl enable docker-compose-app
 
 # start up the application via docker-compose
-#docker-compose -f /srv/docker-compose.yml up -d
+docker-compose -f /srv/docker/docker-compose.yml up -d
